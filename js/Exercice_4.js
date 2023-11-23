d43501e2b27e94aaff9d6c18f2e30e864c3e9748
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-11-23 17:00:56 +0100

    exercice 4

---
+++ js/Exercice_4.js
@@ -0,0 +1,6 @@
+let lastName = prompt("Votre nom de famille :");
+let firstName = prompt("Votre prénom :");
+let city = prompt("Votre ville :");
+
+
+alert("Nom : " + lastName + "\nPrénom : " + firstName + "\nVille : " + city);
