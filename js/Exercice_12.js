ea19de630d2f4e4ea9878c5e15bade6f6bda3e9b
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-11-24 14:56:29 +0100

    exercice 12

---
+++ js/Exercice_12.js
@@ -0,0 +1,8 @@
+function nombreAuCarre(nombre){
+    return nombre ** 2; 
+}
+
+
+let nombreUtilisateur = prompt("saisissez un nombre : ");
+
+alert("Le carré de " + nombreUtilisateur + " est " + nombreAuCarre(nombreUtilisateur));
